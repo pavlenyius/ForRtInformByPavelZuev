e6bf5354c390c440f4b76e8bb4a5a0ee2a9a5cdb
pavlenyius <p.zyev01@gmail.com>
2025-01-23 18:33:55 +0300

    First committ

--- src/ElementCounter.java
+++ src/ElementCounter.java
@@ -13,6 +13,6 @@ public class ElementCounter {
     public static void main(String[] args) {
         int[] arr = {1, 3, 4, 5, 1, 5, 4};
         Map<Integer, Integer> result = countElements(arr);
-        System.out.println(result); // Вывод: {1=2, 3=1, 4=2, 5=2}
+        System.out.println(result);
     }
 }
